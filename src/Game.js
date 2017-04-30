c1e47ae2f069b1882fcd4d662f9bf68424e36bdf
Marc Porciuncula <marcporciuncula@gmail.com>
2017-04-30 22:26:41 +1000

    add links to GitHub (also format with prettier)

--- src/Game.js
+++ src/Game.js
@@ -3,13 +3,7 @@ import { connect } from 'react-redux';
 import R from 'ramda';
 
 import withDimensions from './withDimensions';
-import {
-  RESIZE,
-  SET,
-  TICK,
-  CLEAR,
-  REPLACE,
-} from './reducers/universe';
+import { RESIZE, SET, TICK, CLEAR, REPLACE } from './reducers/universe';
 import './Game.css';
 import '@material/button/dist/mdc.button.css';
 
@@ -28,13 +22,16 @@ class Game extends React.Component {
     this.state = {
       playing: false,
       steps: 0,
-    }
+    };
   }
   componentDidMount() {
     this.handleResize();
   }
   componentWillReceiveProps(props) {
-    if (this.props.width !== props.width || this.props.height !== props.height) {
+    if (
+      this.props.width !== props.width ||
+      this.props.height !== props.height
+    ) {
       setTimeout(this.handleResize, 1);
     }
   }
@@ -54,7 +51,7 @@ class Game extends React.Component {
       this.props.dispatch({ type: TICK });
       this.setState({
         steps: this.state.steps + 1,
-      })
+      });
     }, 1000 / 10);
     this.setState({
       playing: true,
@@ -81,17 +78,20 @@ class Game extends React.Component {
     this.props.dispatch({
       type: REPLACE,
       state: this.props.universe.map(row =>
-        row.map(() => Math.random() < 0.4 ? 1 : 0)
-      )
+        row.map(() => (Math.random() < 0.4 ? 1 : 0)),
+      ),
     });
   }
   render() {
     return (
       <div
         className="universe"
-        style={{ height: this.props.universe.length * CELL_WIDTH, width: this.props.universe[0].length * CELL_WIDTH }}
+        style={{
+          height: this.props.universe.length * CELL_WIDTH,
+          width: this.props.universe[0].length * CELL_WIDTH,
+        }}
       >
-        {this.props.universe.map((row, y) => (
+        {this.props.universe.map((row, y) =>
           row.map((cellAge, x) => (
             <div
               key={`${x}:${y}`}
@@ -103,46 +103,50 @@ class Game extends React.Component {
               }}
               onClick={() => this.toggleCell(x, y)}
             />
-          ))
-        ))}
-        <div className="menu">
-          <button
-            className="mdc-button mdc-button--raised"
-            onClick={this.state.playing ? this.pause : this.play}
-          >
-            {this.state.playing ? 'Pause' : 'Play'}
-          </button>
-          <button
-            className="mdc-button"
-            onClick={this.reset}
-          >
-            Reset
-          </button>
-          <button
-            className="mdc-button"
-            onClick={this.randomize}
-          >
-            Random
-          </button>
-          <button disabled
-            className="mdc-button"
-          >
-            {this.state.steps}
-          </button>
+          )),
+        )}
+        <div className="docked">
+          <div className="menu">
+            <button
+              className="mdc-button mdc-button--raised"
+              onClick={this.state.playing ? this.pause : this.play}
+            >
+              {this.state.playing ? 'Pause' : 'Play'}
+            </button>
+            <button className="mdc-button" onClick={this.reset}>
+              Reset
+            </button>
+            <button className="mdc-button" onClick={this.randomize}>
+              Random
+            </button>
+            <button disabled className="mdc-button">
+              {this.state.steps}
+            </button>
+          </div>
+          <p className="credit">
+            Made by
+            {' '}
+            <a href="https://github.com/MarcoThePoro">Marc Porciuncula</a>
+            .
+            {' '}
+            <a href="https://github.com/MarcoThePoro/fcc-game-of-life">
+              View source on GitHub
+            </a>
+          </p>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export default R.compose(
   connect(
-    (state) => ({
+    state => ({
       universe: state.universe,
     }),
-    (dispatch) => ({
+    dispatch => ({
       dispatch,
     }),
   ),
-  withDimensions
+  withDimensions,
 )(Game);
